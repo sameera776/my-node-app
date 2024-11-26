108932253a29204b91a5b26dac959744609f1e87
VEMULA SAMEERA <141514599+sameera776@users.noreply.github.com>
2024-11-26 22:33:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
     res.send('Hello, Docker!');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server is running on port 3000');
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Server is running on port 3001');
 });
